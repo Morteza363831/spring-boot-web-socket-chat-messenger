e77801ba214e27b09558862f5478fe9a2ac04c68
Morteza363831 <120910275+Morteza363831@users.noreply.github.com>
2024-08-14 00:20:20 +0330

    project initialized and setting for app added !

---
+++ src/main/java/academy/learnprograming/springbootwebsocketchatmessenger/SpringBootWebSocketChatMessengerApplication.java
@@ -0,0 +1,13 @@
+package academy.learnprograming.springbootwebsocketchatmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSocketChatMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebSocketChatMessengerApplication.class, args);
+    }
+
+}
