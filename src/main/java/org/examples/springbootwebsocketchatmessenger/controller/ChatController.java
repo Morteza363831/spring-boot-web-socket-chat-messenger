e1ba19f35c5b989339c5ce95c477aa9829797a50
morteza363831 <morteza363831official@gmail.com>
2024-08-26 00:45:25 +0330

    chat page wil handle here using /chat endpoint

--- src/main/java/org/examples/springbootwebsocketchatmessenger/controller/ChatController.java
+++ src/main/java/org/examples/springbootwebsocketchatmessenger/controller/ChatController.java
@@ -1,14 +1,21 @@
-package academy.learnprograming.springbootwebsocketchatmessenger.controller;
+package org.examples.springbootwebsocketchatmessenger.controller;
 
-import academy.learnprograming.springbootwebsocketchatmessenger.model.ChatMessages;
+import org.examples.springbootwebsocketchatmessenger.entity.ChatMessages;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ChatController {
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String username) {
+        return "chat";
+    }
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
